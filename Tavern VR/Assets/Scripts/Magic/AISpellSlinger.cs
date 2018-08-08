34742412cc3b212073a5d848754846bd4e88bf28
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-08-08 09:48:57 +0800

    SpellScriptAI started

---
+++ Tavern VR/Assets/Scripts/Magic/AISpellSlinger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISpellSlinger : MonoBehaviour {
+    //need a timer
+    //need a nullifiable reference to an object
+    //randomly decide a spell (int 0, 1)
+    //randomly decide a color (int 0, 1, 2)
+    //use animator controller to set states.
+    //on "casting" state, instantiate spell and "grab" it
+    //on "throwing" state, start the animation that uses the function below
+
+    public void UngrabAIAnimFunct() {
+        //reference script in instantiated spell
+    }
+}
