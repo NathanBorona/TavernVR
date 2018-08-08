a18e165698b416dae0f3008a4f344d90f6c29cb0
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-08-08 14:08:12 +0800

    Fixed some stuff

--- Tavern VR/Assets/Scripts/Magic/SpellBall.cs
+++ Tavern VR/Assets/Scripts/Magic/SpellBall.cs
@@ -6,6 +6,7 @@ namespace VRTK {
     public class SpellBall : SpellScript {
         public int damage = 10;
         public float speedMult;
+        bool hasStopped = false;
 
         private void Awake() {
             spellRB = GetComponent<Rigidbody>();
@@ -20,10 +21,17 @@ namespace VRTK {
         }
         protected override void SpellCast() {
             if (targetEnemy != null) {
+                if (!hasStopped) {
+                    spellRB.AddForce(spellRB.mass * -spellRB.velocity);
+                    if (spellRB.velocity.sqrMagnitude <= 0.25f) {
+                        hasStopped = true;
+                    }
+                }
+                else {
+                    spellRB.AddForce(spellRB.mass * speedMult * Vector3.Normalize(targetEnemy.transform.position - transform.position));
+                }
                 //use targetEnemy as target
                 //send ball towards target enemy. no gravity, and correct for overshooting
-                spellRB.AddForce(spellRB.mass * speedMult * Vector3.Normalize(targetEnemy.transform.position - transform.position));
-                spellRB.AddForce(spellRB.mass * 0.5f * -spellRB.velocity);
             }
         }
         private void OnCollisionEnter(Collision collision) {
