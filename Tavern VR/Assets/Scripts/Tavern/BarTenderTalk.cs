cf2eac1921845a38b3aea35d8c574fbeabdfe914
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2018-08-09 10:55:36 +0800

    Tavern
    Nathan

---
+++ Tavern VR/Assets/Scripts/Tavern/BarTenderTalk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarTenderTalk : MonoBehaviour {
+
+    public GameObject headset;
+    public AudioSource barTender;
+    int once = 1;
+
+    void Update()
+    {
+        Conversation();
+    }
+
+    public void Conversation()
+    {
+        if (Vector3.Distance(headset.transform.position, transform.position) < 2f && once == 1)
+        {
+            barTender.Play();
+            Debug.Log("playing");
+            once++;
+        }
+    }
+}
