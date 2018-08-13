f72f25ce791f7aa0d730b63a33d7b8b4ea67c8a8
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2018-08-13 15:03:29 +0800

    Hallucinogenic Cupcakes
    Nathan

---
+++ Tavern VR/Assets/Scripts/Tavern/Cupcake.cs
@@ -0,0 +1,84 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Cupcake : MonoBehaviour
+    {
+
+        public GameObject full;
+        public GameObject bite1;
+        public GameObject bite2;
+        public GameObject bite3;
+        public GameObject blur;
+
+        public GameObject headset;
+        public float blurOut;
+        public float time;
+        public bool offChops;
+
+        // Use this for initialization
+        void Start()
+        {
+
+            full.SetActive(true);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Eating();
+            if (offChops == true)
+            {
+                High();
+            }
+        }
+
+        public void Eating()
+        {
+            if ((headset.transform.position - transform.position).magnitude < 0.5f)
+            {
+                //if Glass is next to headset
+                time = time + Time.deltaTime;
+                if (time >= 4)
+                {
+                    bite3.SetActive(false);
+                    blur.SetActive(true);
+                    offChops = true;
+                }
+                else if (time >= 3)
+                {
+                    bite2.SetActive(false);
+                    bite3.SetActive(true);
+                }
+                else if (time >= 2)
+                {
+                    bite1.SetActive(false);
+                    bite2.SetActive(true);
+                }
+                else if (time >= 1)
+                {
+                    full.SetActive(false);
+                    bite1.SetActive(true);
+                }
+                else if (time >= 0)
+                {
+                    full.SetActive(true);
+                }
+            }
+        }
+
+        public void High ()
+        {
+            blurOut = blurOut + Time.deltaTime;
+            if (blurOut >= 10)
+            {
+                blur.SetActive(false);
+                offChops = false;
+            }
+        }
+
+    }
+}
