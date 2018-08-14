c369bbdc143bc1abd1aecf51a6e797b6f74ce1bd
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2018-08-14 10:43:52 +0800

    Tavern
    Nathan

--- Tavern VR/Assets/Scripts/Tavern/OpenDoorScript.cs
+++ Tavern VR/Assets/Scripts/Tavern/OpenDoorScript.cs
@@ -5,18 +5,45 @@ using UnityEngine.SceneManagement;
 namespace VRTK {
 
 
-    public class OpenDoorScript : VRTK_InteractableObject {
+    public class OpenDoorScript : VRTK_InteractableObject
+    {
 
-        
         public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
         {
             base.StartUsing(currentUsingObject);
             //Change this to the scene that the door will load
-            SceneManager.LoadScene(1);  
+            //SceneManager.LoadScene(1);
             //Debug.Log("ThisIsWorking");
+            if (tag == ("KnightDoor"))
+            {
+                Knight();
+            }
+            if (tag == ("MageDoor"))
+            {
+                Mage();
+            }
+            if (tag == ("Exit"))
+            {
+                Quit();
+            }
 
         }
-    }
 
-    
+        void Quit()
+        {
+            Application.Quit();
+        }
+
+        void Mage()
+        {
+            SceneManager.LoadScene("MageRoom");
+        }
+
+        void Knight()
+        {
+            SceneManager.LoadScene("TestKnights");
+        }
+
+    }
 }
+
