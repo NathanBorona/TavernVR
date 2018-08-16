5d93bed2c389ab24a60500d8b78409711391e471
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2018-08-16 13:20:50 +0800

    shite
    Nath

--- Tavern VR/Assets/Scripts/Knights/SpawnScript.cs
+++ Tavern VR/Assets/Scripts/Knights/SpawnScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour {
 
+    public GameObject Target;
+    public int rnd;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +16,21 @@ public class SpawnScript : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void SpawnPos ()
+    {
+        rnd = Random.Range(1, 4);
+        if (rnd == 1)
+        {
+            Instantiate(Target,new Vector3(-69f, 1.7f, 0f),Quaternion.Euler(0f, 90f, 0f));
+        }
+        if (rnd == 2)
+        {
+            Instantiate(Target, new Vector3(-3f, 1.7f, 5f), Quaternion.Euler(0f, 120f, 0f));
+        }
+        if (rnd == 3)
+        {
+            Instantiate(Target, new Vector3(3f, 1.7f, 5f), Quaternion.Euler(0f, 50f, 0f));
+        }
+    }
 }
