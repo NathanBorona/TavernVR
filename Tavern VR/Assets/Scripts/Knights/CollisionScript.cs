64b33960462401175e9f78f810c5024eba280969
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2018-08-21 00:17:21 +0800

    direction update

--- Tavern VR/Assets/Scripts/Knights/CollisionScript.cs
+++ Tavern VR/Assets/Scripts/Knights/CollisionScript.cs
@@ -10,35 +10,52 @@ public class CollisionScript : MonoBehaviour {
     public targetScript tgtScript;                      //Creating a reference to the TargetScript
     private Vector3 startPos;
     private Vector3 endPos;
+    private float yResA;
+    private float yResM;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "SwordBlade")       //Checking the tag on the object that it coliides with
         {
             startPos = other.transform.position;
+            //startPos.y += yRes;
             //print("TriggerEnter");                    //For debugging 
-            tgtScript.movespeed = 0.20f;                //Slows the speed of the target
+            //tgtScript.movespeed = 0.20f;                //Slows the speed of the target
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-     
+
         if (other.gameObject.tag == "SwordBlade")        //Checking the tag on the object that it coliides with
         {
             endPos = other.transform.position;
-           // Destroy(target, 3f);                        //Destroys the object "Target" after 3s (For performance)
-           // targetBottomLeftRb.isKinematic = false;     //Disables kinematic so it falls apart
+            yResA = startPos.y + yResA + 0.20f;
+            yResM = startPos.y + yResM - 0.20f;
+
+            print(startPos.y + " StartPos");
+            print(yResA + " Added");
+            print(yResM + " Taken");
+            print(endPos.y + " endpos");
+            // Destroy(target, 3f);                        //Destroys the object "Target" after 3s (For performance)
+            // targetBottomLeftRb.isKinematic = false;     //Disables kinematic so it falls apart
             //targetBottomRightRb.isKinematic = false;
-            
+
         }
-        if (other.gameObject.tag == "SwordBlade" && endPos.z < startPos.z || endPos.z > startPos.z)        //Checking the tag on the object that it coliides with
+        if (other.gameObject.tag == "SwordBlade" && (endPos.z > startPos.z || endPos.z < startPos.z) && (endPos.y < yResA && endPos.y > yResM))        //Checking the tag on the object that it coliides with
         {
-        
+            print(startPos.y + " StartPos");
+            print(yResA + " Added");
+            print(yResM + " Taken");
             Destroy(target, 3f);                        //Destroys the object "Target" after 3s (For performance)
             targetBottomLeftRb.isKinematic = false;     //Disables kinematic so it falls apart
             targetBottomRightRb.isKinematic = false;
-
+        }
+        else
+        {
+            startPos = new Vector3(0, 0, 0);
+            yResA = 0f;
+            yResM = 0f;
         }
     }
 
