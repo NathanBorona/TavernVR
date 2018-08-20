749081eae1fff77b3345d29c2e62e82cd46c4b4e
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2018-08-20 17:32:34 +0800

    Starting of Slash direction input

--- Tavern VR/Assets/Scripts/Knights/CollisionScript.cs
+++ Tavern VR/Assets/Scripts/Knights/CollisionScript.cs
@@ -4,31 +4,41 @@ using UnityEngine;
 
 public class CollisionScript : MonoBehaviour {
 
-    public GameObject target;
-    public Rigidbody targetBottomLeftRb;
-    public Rigidbody targetBottomRightRb;
-    public targetScript tgtScript;
-
+    public GameObject target;                           //Creating a reference to the target object
+    public Rigidbody targetBottomLeftRb;                //Creating a reference to the targets Rigidbody (As i'm commenting this i realize i could've used target.gameObject.GetComponentsInChildren<Rigidbody>();)
+    public Rigidbody targetBottomRightRb;               //Creating a reference to the targets Rigidbody (As i'm commenting this i realize i could've used target.gameObject.GetComponentsInChildren<Rigidbody>();)
+    public targetScript tgtScript;                      //Creating a reference to the TargetScript
+    private Vector3 startPos;
+    private Vector3 endPos;
 
     private void OnTriggerEnter(Collider other)
     {
-
-       
-        if (other.gameObject.tag == "SwordBlade")
+        if (other.gameObject.tag == "SwordBlade")       //Checking the tag on the object that it coliides with
         {
-            
-            print("TriggerEnter");
-            tgtScript.movespeed = 0.20f;
+            startPos = other.transform.position;
+            //print("TriggerEnter");                    //For debugging 
+            tgtScript.movespeed = 0.20f;                //Slows the speed of the target
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.tag == "SwordBlade")
+     
+        if (other.gameObject.tag == "SwordBlade")        //Checking the tag on the object that it coliides with
         {
-            Destroy(target, 3f);
-            targetBottomLeftRb.isKinematic = false;
+            endPos = other.transform.position;
+           // Destroy(target, 3f);                        //Destroys the object "Target" after 3s (For performance)
+           // targetBottomLeftRb.isKinematic = false;     //Disables kinematic so it falls apart
+            //targetBottomRightRb.isKinematic = false;
+            
+        }
+        if (other.gameObject.tag == "SwordBlade" && endPos.z < startPos.z || endPos.z > startPos.z)        //Checking the tag on the object that it coliides with
+        {
+        
+            Destroy(target, 3f);                        //Destroys the object "Target" after 3s (For performance)
+            targetBottomLeftRb.isKinematic = false;     //Disables kinematic so it falls apart
             targetBottomRightRb.isKinematic = false;
+
         }
     }
 
