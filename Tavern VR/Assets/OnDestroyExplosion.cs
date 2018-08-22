a81697811c722ec127e9ce7b8a641f40ec5afa48
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-08-22 12:49:32 +0800

    holy shit

---
+++ Tavern VR/Assets/OnDestroyExplosion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnDestroyExplosion : MonoBehaviour {
+    GameObject mySplodeFect;
+    AudioSource mySource;
+    AudioSource splodeSource;
+    public AudioClip myDeathClip;
+
+    private void OnDestroy() {
+        mySplodeFect = new GameObject("mySplode");
+        mySplodeFect.AddComponent<AudioSource>();
+        splodeSource = mySplodeFect.GetComponent<AudioSource>();
+        splodeSource.volume = 0.25f;
+        splodeSource.PlayOneShot(myDeathClip);
+    }
+}
